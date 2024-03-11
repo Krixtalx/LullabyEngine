81cc94c2cdf387006fc930b2622e621666bbd3c7
José Antonio <42522226+Krixtalx@users.noreply.github.com>
2024-03-11 14:53:48 +0100

    Update Typedefs.h

--- LullabyCore/Source/Lullaby/Typedefs.h
+++ LullabyCore/Source/Lullaby/Typedefs.h
@@ -1,17 +1,20 @@
 #pragma once
 
-typedef glm::vec4 vec4;
-typedef glm::mat4 mat4;
-
-typedef glm::vec3 vec3;
-typedef glm::mat3 mat3;
-
 typedef glm::vec2 vec2;
-typedef glm::uvec3 uvec3;
-typedef glm::uvec2 uvec2;
-
-typedef glm::ivec3 ivec3;
+typedef glm::vec3 vec3;
+typedef glm::vec4 vec4;
 typedef glm::ivec2 ivec2;
+typedef glm::ivec3 ivec3;
+typedef glm::ivec4 ivec4;
+typedef glm::uvec2 uvec2;
+typedef glm::uvec3 uvec3;
+typedef glm::uvec4 uvec4;
+
+typedef glm::mat2 mat2x2;
+typedef glm::mat3 mat3x3;
+typedef glm::mat4 mat4x4;
+typedef glm::mat3x4 mat3x4;
+typedef glm::mat4x3 mat4x3;
 
 typedef glm::quat quat;
 
